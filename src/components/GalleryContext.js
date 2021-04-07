87636a9c2a97eb1a8c20391c5b937756f99f5cb3
Ruairi Conway <hi@ruairiconway.com>
2021-04-07 18:03:09 -0400

    created gallery context

---
+++ src/components/GalleryContext.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+
+const GalleryContext = React.createContext()
+
+function GalleryContextProvider({children}) {
+
+    const [ galleryData, setGalleryData ] = useState({})
+    const galleryAPI = `https://mysterious-dawn-90465.herokuapp.com/gallery`
+
+    useEffect(() => {
+        fetch(`${galleryAPI}/collections/58565055/photos?per_page=10`)
+            .then(res => res.json())
+            .then(resJson => setGalleryData(resJson))
+            .catch(err => console.error(err))
+    }, [])
+
+    console.log(galleryData)
+
+    return (
+        <GalleryContext.Provider value={galleryData}>
+            {children}
+        </GalleryContext.Provider>
+    )
+}
+
+export { GalleryContextProvider, GalleryContext }
