7797bb378d1efdad60380e93427d83bba3a82635
Ruairi Conway <hi@ruairiconway.com>
2021-04-12 13:43:37 -0400

    added toggleFavorite method

--- src/components/GalleryContext.js
+++ src/components/GalleryContext.js
@@ -5,8 +5,8 @@ const GalleryContext = React.createContext()
 function GalleryContextProvider({children}) {
 
     const [ galleryData, setGalleryData ] = useState([])
-    const galleryAPI = `https://mysterious-dawn-90465.herokuapp.com/gallery`
 
+    const galleryAPI = `https://mysterious-dawn-90465.herokuapp.com/gallery`
     useEffect(() => {
         fetch(`${galleryAPI}/collections/58565055/photos?per_page=10`)
             .then(res => res.json())
@@ -14,8 +14,24 @@ function GalleryContextProvider({children}) {
             .catch(err => console.error(err))
     }, [])
 
+    function toggleFavorite(id) {
+        const updatedGallery = galleryData.map(image => {
+            if (image.id === id) {
+                console.log(id)
+                console.log(!image.isFavorite)
+                return {
+                    ...image,
+                    isFavorite: !image.isFavorite
+                }
+            } else {
+                return image
+            }
+        })
+        setGalleryData(updatedGallery)
+    }
+
     return (
-        <GalleryContext.Provider value={{galleryData}}>
+        <GalleryContext.Provider value={{galleryData, toggleFavorite}}>
             {children}
         </GalleryContext.Provider>
     )
