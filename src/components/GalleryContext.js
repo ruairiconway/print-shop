dda5031a8da8dc6a9a54af7f6b09cb3a3885a716
Ruairi Conway <hi@ruairiconway.com>
2021-04-12 17:31:16 -0400

    added cart and pricing functionality

--- src/components/GalleryContext.js
+++ src/components/GalleryContext.js
@@ -4,24 +4,43 @@ const GalleryContext = React.createContext()
 
 function GalleryContextProvider({children}) {
 
+    // fetch images
     const [ galleryData, setGalleryData ] = useState([])
-
     const galleryAPI = `https://mysterious-dawn-90465.herokuapp.com/gallery`
     useEffect(() => {
         fetch(`${galleryAPI}/collections/58565055/photos?per_page=10`)
             .then(res => res.json())
-            .then(resJson => setGalleryData(resJson))
+            .then(resJson => {
+                setGalleryData(resJson)
+                setGalleryPrice(resJson)
+            })
             .catch(err => console.error(err))
     }, [])
 
+    // set prices
+    function setGalleryPrice(data) {
+        for (let item of data) {
+            item.price = item.height > item.width ? 50 : 75
+        }
+    }
+
+    // cart state
+    const [ cartItems, setCartItems ] = useState([])
+    function addToCart(imageData) {
+        setCartItems(prevImageData => [...prevImageData, imageData])
+    }
+    function removeFromCart(imageData) {
+        const currentGallery = cartItems.filter(cartItem => cartItem.id !== imageData.id)
+        setCartItems(currentGallery)
+    }
+
+    // toggle favorite
     function toggleFavorite(id) {
         const updatedGallery = galleryData.map(image => {
             if (image.id === id) {
-                console.log(id)
-                console.log(!image.isFavorite)
                 return {
                     ...image,
-                    isFavorite: !image.isFavorite
+                    isFavorite: !image.isFavorite,
                 }
             } else {
                 return image
@@ -31,7 +50,7 @@ function GalleryContextProvider({children}) {
     }
 
     return (
-        <GalleryContext.Provider value={{galleryData, toggleFavorite}}>
+        <GalleryContext.Provider value={{galleryData, toggleFavorite, cartItems, addToCart, removeFromCart}}>
             {children}
         </GalleryContext.Provider>
     )
