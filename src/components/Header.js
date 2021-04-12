c0a4f35131c4b53ff3b86289affdb62e6d5f120b
Ruairi Conway <hi@ruairiconway.com>
2021-04-12 17:30:47 -0400

    added cart functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { GalleryContext } from './GalleryContext'
 
 function Header(props) {
+    const { cartItems } = useContext(GalleryContext)
+
+    const cartIconClass = cartItems.length > 0 ? 'fill' : 'line' 
+    
     return (
         <div className="header">
             <Link to='/'>
                 <h1>PRINT SHOP</h1>
             </Link>
             <Link to='/cart'>
-                <i className="ri-shopping-bag-2-line"></i>
+                <i className={`ri-shopping-bag-2-${cartIconClass}`}></i>
             </Link>
         </div>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -4,24 +4,32 @@ import { GalleryContext } from './GalleryContext'
 
 function Image({ data }) {
     const [ isHovered, setIsHovered ] = useState(false)
-    const { toggleFavorite } = useContext(GalleryContext)
+    const { toggleFavorite, cartItems, addToCart, removeFromCart } = useContext(GalleryContext)
 
-    function handleHeartIcon() {
+    function heartIcon() {
         if (data.isFavorite) {
             return <i className="ri-heart-2-fill heart" onClick={() => toggleFavorite(data.id)}></i>
         } else if (isHovered) {
             return <i className="ri-heart-2-line heart" onClick={() => toggleFavorite(data.id)}></i>
         }
     }
-    const cartIcon = isHovered && <i className="ri-shopping-cart-line cart"></i>
+
+    function cartIcon() {
+        const isInCart = cartItems.some(item => item.id === data.id)
+        if (isInCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(data)}></i>
+        } else if (isHovered) {
+            return <i className="ri-shopping-cart-line cart" onClick={() => addToCart(data)}></i>
+        }
+    }
 
     return (
         <div className='image-wrapper'
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >   
-            {handleHeartIcon()}
-            {cartIcon}
+            {heartIcon()}
+            {cartIcon()}
             <img
                 src={data.urls.small}
                 alt={data.alt_description}
